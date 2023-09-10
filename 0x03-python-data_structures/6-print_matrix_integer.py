ea19a8e892cdd21e210f8d5c5d10991d7cf146b9
yahyarashed <yahyarashed98@gmail.com>
2023-09-10 09:13:52 -0700

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for submatrix in matrix:
+        if len(submatrix) == 0:
+            print()
+        for i in range(len(submatrix)):
+            print('{:d}' .format(submatrix[i]),
+                  end="\n" if i is len(submatrix) - 1 else " ")
