fe8fc70dcf7dc6249403f165e1dd3fc59e6e0152
yehia <yahyarashed98@gmail.com>
2023-11-27 12:58:18 -0800

    real rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""module that contains a Rectangle class"""
+
+
+class Rectangle:
+    """a blueprint that accesses and manipulate the data in a Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """class attributes"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """a getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """a setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """a getter for height"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """a setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
