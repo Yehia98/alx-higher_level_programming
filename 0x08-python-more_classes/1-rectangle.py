ec21c94b1c68a73228e9c37cec83e4726661dfd5
yehia <yahyarashed98@gmail.com>
2023-11-27 13:08:49 -0800

    real rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """a blueprintate of a Rectangle class"""
     def __init__(self, width=0, height=0):
         """class attributes"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
