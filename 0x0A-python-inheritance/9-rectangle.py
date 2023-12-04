e73edf1fc9c01808ab43a35f7ffc3830226587a3
yehia <yahyarashed98@gmail.com>
2023-12-04 14:50:22 -0800

    rectangle area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a module of Rectangle subclass"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """a sub class of BaseGeometry super class"""
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """a function that calculates the area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """printing a string"""
+        return "[Rectangle]" + str(self.__width) + "/" + str(self.__height)
