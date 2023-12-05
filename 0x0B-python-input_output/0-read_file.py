57bc0c02930aa2817478f89bb8581f98fc08cb46
yehia <yahyarashed98@gmail.com>
2023-12-05 14:08:34 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a modue of a file read function"""
+
+
+def read_file(filename=""):
+    """a function that reads the contents of a file"""
+    with open(filename, 'r', encoding = "utf-8") as file:
+        file_content = file.read()
+    print(file_content)
